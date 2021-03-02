f2d531a5b5cbad8f33dc277e91272bab9bdcf5d6
Omar Elassal <omarhanea2@gmail.com>
2021-03-02 03:11:28 +0200

    Update js.js

--- js.js
+++ js.js
@@ -7,12 +7,5 @@
         x.className = "topnav";
       }
     }
-    </script>
-    <script src='https://storage.ko-fi.com/cdn/scripts/overlay-widget.js'></script>
-    <script>
-      kofiWidgetOverlay.draw('omarelassal', {
-        'type': 'floating-chat',
-        'floating-chat.donateButton.text': 'Get Me Kofi',
-        'floating-chat.donateButton.background-color': '#00bfa5',
-        'floating-chat.donateButton.text-color': '#fff'
-      });
+
+    
