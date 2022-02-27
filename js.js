8c98d961c85b27fe69ab160979c7b187ed37d007
Omar Elassal <omarhanea2@gmail.com>
2022-02-27 16:27:27 +0200

    Update js.js

--- js.js
+++ js.js
@@ -1,11 +1,1 @@
-
- function myFunction() {
-      var x = document.getElementById("myTopnav");
-      if (x.className === "topnav") {
-        x.className += " responsive";
-      } else {
-        x.className = "topnav";
-      }
-    }
-
-    
+function myFunction(){var a=document.getElementById("myTopnav");"topnav"===a.className?a.className+=" responsive":a.className="topnav"}
